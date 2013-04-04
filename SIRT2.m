77a905b9b6f37656b2da016ca58c906b6a2d0c16
Yisong Chiao <yis.qiao@gmail.com>
2013-04-04 16:55:08 +0200

    SIRT method in 2D

--- SIRT2.m
+++ SIRT2.m
@@ -1,6 +1,15 @@
 function [x] = SIRT2(A,b,k)
+% SIRT2 - Simultaneous Iterative Reconstruction Technique (SIRT) 
+%         method in 2D images
+%   it reconstruct the 2D image using SIRT 
 
-[m,n] = size(A); 
+% parameter:
+%   A, SystemMatrix
+%   b, projection
+%   k, iteration times
+%   x, result
+
+[~,n] = size(A); 
 sigma1tilde = svds(A,1);
 lambda = 1/sigma1tilde^2;
 % Prepare for iteration.
