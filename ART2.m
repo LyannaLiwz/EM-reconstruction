f280e98a7d5df981b7823ab96be1c8db1a85355c
Yisong Chiao <yis.qiao@gmail.com>
2013-04-04 16:49:55 +0200

    ART 2D method

--- ART2.m
+++ ART2.m
@@ -1,6 +1,15 @@
 function [x] = ART2(A,b,lambda,k)
+% ART2 -  Algebraic Reconstruction Technique (ART) method in 2D images
+%   it reconstruct the 2D image using ART 
 
-[m,n] = size(A); 
+% parameter:
+%   A, SystemMatrix
+%   R, projection
+%   lambda, constance
+%   k, iteration times
+%   x, result
+
+[~,n] = size(A);
 % Prepare for iteration.
 x = zeros(n,1);
 nai2 = full(sum(abs(A.*A),2));
@@ -12,4 +21,4 @@ for j=1:k
       Ai = full(A(i,:));
       x = x + lambda*(b(i)-Ai*x)*Ai'/nai2(i);
    end
-end
+end 
